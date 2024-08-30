ee1a7c3a3f45b1c06957d2480bfe25aabf8a6ab2
Nicolas Fedor <fedronn@gmail.com>
2024-08-30 15:19:05 +0100

    refactor: remove unused index_to_coord function

--- src/utils.rs
+++ src/utils.rs
@@ -3,11 +3,3 @@ pub fn coord_to_index(position: glam::UVec3, size: glam::UVec3) -> usize {
         + position.y as usize * size.x as usize
         + position.z as usize * size.x as usize * size.y as usize
 }
-
-pub fn index_to_coord(index: usize, size: glam::UVec3) -> glam::UVec3 {
-    glam::uvec3(
-        index as u32 % size.x,
-        (index as u32 / size.x) % size.y,
-        index as u32 / (size.x * size.y),
-    )
-}
