8dfe275375470591bb3353cda016b9fb5605e472
Nicolas Fedor <fedronn@gmail.com>
2024-08-30 18:24:28 +0100

    perf: multi-thread chunk meshing

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
+use crossbeam_skiplist::SkipMap;
 use glium::{DrawParameters, Surface};
+use rayon::prelude::*;
 
 use crate::{
     app::Window,
@@ -26,54 +28,81 @@ impl World {
     pub fn new(window: &Window, generator: &WorldGenerator) -> Self {
         let world = generator.generate_world();
 
+        let solid_meshes = SkipMap::new();
+        let transparent_meshes = SkipMap::new();
+
+        world
+            .par_iter()
+            .filter(|(_, chunk)| !chunk.is_empty())
+            .for_each(|(&position, chunk)| {
+                let mut neighbours = HashMap::new();
+
+                for axis in [Axis::X, Axis::Y, Axis::Z] {
+                    for direction in [Direction::Positive, Direction::Negative] {
+                        let neighbour_position =
+                            position.as_ivec3() + axis.get_normal(direction).as_ivec3();
+                        let neighbour_position: Result<glam::UVec3, _> =
+                            neighbour_position.try_into();
+                        if let Ok(neighbour_position) = neighbour_position {
+                            if let Some(neighbour) = world.get(&neighbour_position) {
+                                neighbours.insert(neighbour_position, neighbour);
+                            }
+                        }
+                    }
+                }
+
+                let (solid_mesh, transparent_mesh) = ChunkMesher::mesh(chunk, &neighbours);
+
+                solid_meshes.insert((position.x, position.y, position.z), solid_mesh);
+                if let Some(transparent_mesh) = transparent_mesh {
+                    transparent_meshes
+                        .insert((position.x, position.y, position.z), transparent_mesh);
+                }
+            });
+
         let mut chunk_solid_buffers = HashMap::new();
         let mut chunk_transparent_buffers = HashMap::new();
         let mut chunk_uniforms = HashMap::new();
 
-        for (&position, chunk) in world.iter() {
-            if chunk.is_empty() {
-                continue;
-            }
+        for mesh in solid_meshes.iter() {
+            let position = {
+                let position = mesh.key();
+                glam::UVec3::new(position.0, position.1, position.2)
+            };
 
-            let mut neighbours = HashMap::new();
+            let mesh = mesh.value();
 
-            for axis in [Axis::X, Axis::Y, Axis::Z] {
-                for direction in [Direction::Positive, Direction::Negative] {
-                    let neighbour_position =
-                        position.as_ivec3() + axis.get_normal(direction).as_ivec3();
-                    let neighbour_position: Result<glam::UVec3, _> = neighbour_position.try_into();
-                    if let Ok(neighbour_position) = neighbour_position {
-                        if let Some(neighbour) = world.get(&neighbour_position) {
-                            neighbours.insert(neighbour_position, neighbour);
-                        }
-                    }
-                }
-            }
-
-            let (solid_mesh, transparent_mesh) = ChunkMesher::mesh(chunk, &neighbours);
             chunk_solid_buffers.insert(
                 position,
                 (
-                    solid_mesh.vertex_buffer(&window.display).unwrap(),
-                    solid_mesh.index_buffer(&window.display).unwrap(),
+                    mesh.vertex_buffer(&window.display).unwrap(),
+                    mesh.index_buffer(&window.display).unwrap(),
                 ),
             );
 
-            if let Some(transparent_mesh) = transparent_mesh {
-                chunk_transparent_buffers.insert(
-                    position,
-                    (
-                        transparent_mesh.vertex_buffer(&window.display).unwrap(),
-                        transparent_mesh.index_buffer(&window.display).unwrap(),
-                    ),
-                );
-            }
-
+            let chunk = world.get(&position).unwrap();
             chunk_uniforms.insert(
                 position,
                 (
                     chunk.transform().model_matrix().to_cols_array_2d(),
                     chunk.transform().normal_matrix().to_cols_array_2d(),
+                ),
+            );
+        }
+
+        for mesh in transparent_meshes.iter() {
+            let position = {
+                let position = mesh.key();
+                glam::UVec3::new(position.0, position.1, position.2)
+            };
+
+            let mesh = mesh.value();
+
+            chunk_transparent_buffers.insert(
+                position,
+                (
+                    mesh.vertex_buffer(&window.display).unwrap(),
+                    mesh.index_buffer(&window.display).unwrap(),
                 ),
             );
         }
